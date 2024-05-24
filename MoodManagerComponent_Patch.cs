7a91b52dbe9f1e591251f8a1087f4e73b20fcda2
iZastic <izastic@gmail.com>
2024-05-24 17:57:53 -0600

    Forgot to add file

---
+++ MoodManagerComponent_Patch.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using ProjectM.Presentation;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+namespace RemoveVignette
+{
+    [HarmonyPatch]
+    internal static class MoodManagerComponent_Patch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MoodManagerComponent), nameof(MoodManagerComponent.OnEnable))]
+        private static void OnEnable(MoodManagerComponent __instance)
+        {
+            var sppTransform = __instance.transform.FindChild("Scene PostProcess");
+            var volume = sppTransform.GetComponent<Volume>();
+            foreach (var vc in volume.profile.components)
+            {
+                if (vc.name.StartsWith("Vignette"))
+                {
+                    vc.active = false;
+                    break;
+                }
+            }
+        }
+    }
+}
